b495b67fa147584ce57f281cc2f517242ba0fe75
dough10 <doughten.jimi@gmail.com>
2021-01-24 11:31:23 -0600

    U: remove no longer needed files, V: 1.0.0

--- src/js/admin.js
+++
@@ -1,7 +0,0 @@
-window.onload = _ => {
-  var socket = new WebSocket(`wss://${window.location.host}/api`);
-  socket.onmessage = event => {
-    const data = JSON.parse(event.data);
-    console.log(data);
-  };
-};
