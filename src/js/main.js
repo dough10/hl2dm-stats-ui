bd003623f2a139dfc5c7c10797d3b37b6dffa411
dough10 <doughten.jimi@gmail.com>
2021-02-01 11:42:32 -0600

    U: wrong, V: 1.0.0

--- src/js/main.js
+++ src/js/main.js
@@ -910,7 +910,7 @@ function makeOption(option, value, parent) {
  */
 function fetchOldMonths(month, cb) {
   console.log(month);
-  if (!month !== undefined) {
+  if (month !== undefined) {
     qs('#months').innerHTML = '';
     fetch('/api/old-months').then(response => {
       if (response.status !== 200) {
