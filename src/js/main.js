1560c4cccb5c3297c28270b18e5c73e15bae6fd4
dough10 <doughten.jimi@gmail.com>
2021-02-19 07:42:09 -0600

    U: missing i, V: 1.0.0

--- src/js/main.js
+++ src/js/main.js
@@ -60,14 +60,16 @@ function loadRipples() {
 function cascadeCards(container) {
   return new Promise(resolve => {
     const cards = qsa('.card', container);
+    let last;
     for (let i = 0; i < cards.length; i++) {
       cards[i].style.display = 'block';
       animations.animateElement(cards[i], 'translateX(0)', 200, 1, i * 50);
+      last = i;
     }
     const nocard = qs('.nocard', container);
     if (!nocard) return;
     nocard.style.display = 'block';
-    animations.animateElement(nocard, 'translateX(0)', 200, 1, i * 50);
+    animations.animateElement(nocard, 'translateX(0)', 200, 1, last * 50);
     resolve();
   });
 }
