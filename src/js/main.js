11d1a194808d101a04de53ae295d25abf9df43ba
dough10 <doughten.jimi@gmail.com>
2022-02-02 17:33:17 -0600

    ??

--- src/js/main.js
+++ src/js/main.js
@@ -58,18 +58,14 @@ function loadRipples() {
 function cascadeCards(container) {
   return new Promise(resolve => {
     const cards = qsa('.card', container);
-    let index = 0;
-    if (cards.length) {
-      cards.map(card, i => {
-        index = i;
-        card.style.display = 'block';
-        animations.animateElement(card, 'translateX(0)', 200, 1, i * 50);  
-      });
+    for (var i = 0; i < cards.length; i++) {
+      cards[i].style.display = 'block';
+      animations.animateElement(cards[i], 'translateX(0)', 200, 1, i * 50);
     }
     const nocard = qs('.nocard', container);
     if (!nocard) return;
     nocard.style.display = 'block';
-    animations.animateElement(nocard, 'translateX(0)', 200, 1, index * 50);
+    animations.animateElement(nocard, 'translateX(0)', 200, 1, i * 50);
     resolve();
   });
 }
